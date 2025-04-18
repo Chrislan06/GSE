2b843218b5e1c81fb2b7a99ebda9bc4472636af3
chrislan <chrislan06@gmail.com>
2025-04-18 14:00:14 -0300

    configurando rota principal de produtos

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def listar_produtos(request):
+    return render(request ,'home.html')
