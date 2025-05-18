40245191e0069aa0a478dd765ae5c6d07a7f768d
chrislan <chrislan06@gmail.com>
2025-05-18 20:53:00 -0300

    views: Criando Rotas e views para CRUD de produtos e categorias

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    path('', views.listar_produtos, name="listar_produtos" )
+    # Produtos
+    path('', views.product_list, name='lista_produtos'),
+    path('produtos/novo/', views.product_create, name='novo_produto'),
+    path('produtos/editar/<int:pk>/', views.product_update, name='editar_produto'),
+    path('produtos/excluir/<int:pk>/', views.product_delete, name='excluir_produto'),
+
+    # Categorias
+    path('categorias/', views.category_list, name='lista_categorias'),
+    path('categorias/novo/', views.category_create, name='nova_categoria'),
+    path('categorias/editar/<int:pk>/', views.category_update, name='editar_categoria'),
+    path('categorias/excluir/<int:pk>/', views.category_delete, name='excluir_categoria'),
+
+    # Relatorios
+    path('produtos/relatorios/', views.reports, name='relatorios'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,8 +1,93 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Product, Category
+from .forms import ProductForm, CategoryForm
 
+
+# Produtos
+@login_required
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'produtos/product_list.html', {'products': products})
 
 @login_required
-def listar_produtos(request):
-    return render(request ,'home.html')
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProductForm()
+    return render(request, 'produtos/product_form.html', {'form': form})
+
+@login_required
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'produtos/product_form.html', {'form': form})
+
+@login_required
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('lista_produtos')
+    return render(request, 'produtos/product_confirm_delete.html', {'product': product})
+
+# Categorias
+@login_required
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'produtos/category_list.html', {'categories': categories})
+
+@login_required
+def category_create(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_categorias')
+    else:
+        form = CategoryForm()
+    return render(request, 'produtos/category_form.html', {'form': form})
+
+@login_required
+def category_update(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_categorias')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'produtos/category_form.html', {'form': form})
+
+@login_required
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('lista_categorias')
+    return render(request, 'produtos/category_confirm_delete.html', {'category': category})
+
+
+# Relatorios
+@login_required
+def reports(request):
+    # Placeholder for reports view
+    products_count = Product.objects.count()
+    categories_count = Category.objects.count()
+    return render(request, 'produtos/reports.html', {
+        'products_count': products_count,
+        'categories_count': categories_count
+    })
+
+
